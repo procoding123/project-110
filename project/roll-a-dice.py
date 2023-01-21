7d909acd9cb5be9c7c9d0cc390e29afa9feb90c7
procoding123 <96044411+procoding123@users.noreply.github.com>
2023-01-21 19:39:55 +0530

    Add files via upload

---
+++ project/roll-a-dice.py
@@ -0,0 +1,19 @@
+import random
+
+response="y"
+while response=="y":
+    no=random.randint(1,6)
+    if no==1:
+        print("1")
+    if no==2:
+        print("2")
+    if no==3:
+        print("3")
+    if no==4:
+        print("4")
+    if no==5:
+        print("5")
+    if no==6:
+        print("6") 
+    response=str(input("Do you want to roll the dice again?"))
+     
